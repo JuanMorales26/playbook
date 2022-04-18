a2e17f0f2552e3678c0c539e53eb2fe3bbe70ab7
Juan Morales <99295980+JuanMorales26@users.noreply.github.com>
2022-04-18 15:50:20 -0500

    Creando mi ejercicio 2

---
+++ weekly_mission_2/Exercises/example_2.js
@@ -0,0 +1,7 @@
+const myObjetc = {
+    name: "Juancho",
+    age: 26,
+    pets: ["Gatos","Perros","Hammsters"],
+}
+console.log("Ejercicio 2: Creamos e imprimimos un objeto con propiedades")
+console.log(myObjetc)
