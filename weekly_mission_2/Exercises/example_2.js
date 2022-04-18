222e0575d99d059b762ebeb62fc32dc53102007e
Juan Morales <99295980+JuanMorales26@users.noreply.github.com>
2022-04-18 15:56:17 -0500

    Update example_2.js

--- weekly_mission_2/Exercises/example_2.js
+++ weekly_mission_2/Exercises/example_2.js
@@ -1,7 +1,11 @@
-const myObjetc = {
+const myID = {
     name: "Juancho",
     age: 26,
     pets: ["Gatos","Perros","Hammsters"],
 }
 console.log("Ejercicio 2: Creamos e imprimimos un objeto con propiedades")
 console.log(myObjetc)
+
+const myID1 = new myID() // Puedo crear muchos objetos de la clase myID
+console.log("Ejercicio 2: Crear un objeto a partir de una clase")
+console.log(myID1) // un objeto de la clase Pet
